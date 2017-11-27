e8850aed0727d76467cf61e315ade81d830dda16
pf <33946003+15651037763@users.noreply.github.com>
2017-11-27 15:33:41 +0800

    Update DemoController.java

--- src/main/java/com/pf/org/cms/web/DemoController.java
+++ src/main/java/com/pf/org/cms/web/DemoController.java
@@ -3,21 +3,23 @@ package com.pf.org.cms.web;
 import com.pf.org.cms.entity.Demo;
 import com.pf.org.cms.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping(value =  "/demo")
 public class DemoController {
     @Autowired
     DemoService demoService;
 
     @RequestMapping(value = "/getAll")
-    public Object testDemo() {
-        List<Demo> ls = demoService.getDemos();
-        System.out.println(ls.toString());
-        return ls;
+    public String testDemo(Map<String,Object> map) {
+        List<Demo> demos = demoService.getDemos();
+        map.put("data", demos);
+        System.out.println(demos.toString());
+        return ("/testDemo");
     }
 }
