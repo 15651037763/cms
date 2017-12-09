888446c86df4370011f2d111c9213ad4be7f844e
pf <33946003+15651037763@users.noreply.github.com>
2017-12-09 19:25:01 +0800

    Create UserServiceImpl.java

---
+++ src/main/java/com/pf/org/cms/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.pf.org.cms.service.impl;
+
+import com.pf.org.cms.entity.UserInfo;
+import com.pf.org.cms.mapper.UserInfoMapper;
+import com.pf.org.cms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service(value = "userService")
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+    @Override
+    public List<UserInfo> getUserInfos(Map<String, Object> paramMap) {
+        paramMap.put("state", 1);
+        return userInfoMapper.getUserInfos(paramMap);
+    }
+}
