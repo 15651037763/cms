db91af9655c47e32bd00c1eb6ccd8f4a67c5648e
pf <33946003+15651037763@users.noreply.github.com>
2017-12-09 19:23:34 +0800

    Create UserInfo.java

---
+++ src/main/java/com/pf/org/cms/entity/UserInfo.java
@@ -0,0 +1,62 @@
+package com.pf.org.cms.entity;
+
+import java.io.Serializable;
+
+public class UserInfo extends BaseEntity implements Serializable {
+    private long id;
+    private String userCode;
+    private String userName;
+    private String userPwd;
+    private String remark;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+                "id=" + id +
+                ", userCode='" + userCode + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userPwd='" + userPwd + '\'' +
+                ", remark='" + remark + '\'' +
+                '}';
+    }
+}
