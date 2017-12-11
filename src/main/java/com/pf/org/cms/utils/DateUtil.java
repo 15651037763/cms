3547ffe4950909f35f406fe78ce5f3e48075d31a
pf <33946003+15651037763@users.noreply.github.com>
2017-12-11 15:03:55 +0800

    Update DateUtil.java

--- src/main/java/com/pf/org/cms/utils/DateUtil.java
+++ src/main/java/com/pf/org/cms/utils/DateUtil.java
@@ -1,5 +1,31 @@
 package com.pf.org.cms.utils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateUtil {
-//    private static int a = 1;
+    public static final String DateTime24h = "yyyy-MM-dd HH:mm:ss";
+    public static final String DateTime12h = "yyyy-MM-dd hh:mm:ss";
+    public static final String dateFormat = "yyyy-MM-dd";
+    public static final String timeFormat = "HH:mm:ss";
+
+    /**
+     * 格式化Date to yyyy-MM-dd HH:mm:ss
+     * @return String
+     */
+    public static String dateTimeToString(Date dateTime) throws Exception{
+        DateFormat df = new SimpleDateFormat(DateTime24h);
+        return df.format(dateTime);
+    }
+
+    /**
+     * 格式化yyyy-MM-dd HH:mm:ss to Date
+     * @return Date
+     */
+    public static Date stringToDateTime(String dateTime) throws Exception{
+        DateFormat df = new SimpleDateFormat(DateTime24h);
+        return df.parse(dateTime);
+    }
+
 }
