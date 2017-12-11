d0c6e8df6232c3019e786b891070fe37daee5237
pf <33946003+15651037763@users.noreply.github.com>
2017-12-11 15:05:53 +0800

    增加服务

--- src/main/java/com/pf/org/cms/mapper/UserInfoMapper.java
+++ src/main/java/com/pf/org/cms/mapper/UserInfoMapper.java
@@ -1,10 +1,23 @@
 package com.pf.org.cms.mapper;
 
 import com.pf.org.cms.entity.UserInfo;
+import com.pf.org.cms.entity.UserRoleInfo;
 
 import java.util.List;
 import java.util.Map;
 
 public interface UserInfoMapper {
+    /**
+     * 获取用户信息
+     * @param paramMap
+     * @return List<UserInfo>
+     */
     List<UserInfo> getUserInfos(Map<String, Object> paramMap);
+
+    /**
+     * 获取用户角色信息
+     * @param paramMap
+     * @return List<UserRoleInfo>
+     */
+    List<UserRoleInfo> getUserRoleInfos(Map<String, Object> paramMap);
 }
