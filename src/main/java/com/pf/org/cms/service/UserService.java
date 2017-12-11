2a253286323f750b8f7782e08996f83b0bb734ec
pf <33946003+15651037763@users.noreply.github.com>
2017-12-11 15:04:39 +0800

    增加服务

--- src/main/java/com/pf/org/cms/service/UserService.java
+++ src/main/java/com/pf/org/cms/service/UserService.java
@@ -1,6 +1,7 @@
 package com.pf.org.cms.service;
 
 import com.pf.org.cms.entity.UserInfo;
+import com.pf.org.cms.entity.UserRoleInfo;
 
 import java.util.List;
 import java.util.Map;
@@ -9,7 +10,14 @@ public interface UserService {
     /**
      * 获取用户信息
      * @param paramMap
-     * @return
+     * @return List<UserInfo>
      */
     public List<UserInfo> getUserInfos(Map<String, Object> paramMap);
+
+    /**
+     * 获取用户角色信息
+     * @param paramMap
+     * @return List<UserRoleInfo>
+     */
+    public List<UserRoleInfo> getUserRoleInfos(Map<String, Object> paramMap);
 }
