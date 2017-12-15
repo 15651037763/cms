8dcf392c13ac5e95dbe7392f492a149b58e1ebbd
pf <33946003+15651037763@users.noreply.github.com>
2017-12-15 16:12:40 +0800

    Create IConstants.java

---
+++ src/main/java/com/pf/org/cms/common/IConstants.java
@@ -0,0 +1,14 @@
+package com.pf.org.cms.common;
+
+/**
+ * @Auther: pf
+ * @Date: 2017/12/14 16:44
+ * @Description:
+ */
+public class IConstants {
+    // 返回码,成功
+    public static final int RESULT_INT_SUCCESS = 0;
+
+    // 返回码,失败
+    public static final int RESULT_INT_ERROR = 1;
+}
