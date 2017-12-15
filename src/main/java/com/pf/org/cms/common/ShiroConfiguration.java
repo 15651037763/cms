4e3ff82442a0c8f19aa17fcbd63c27e19a4132c3
pf <33946003+15651037763@users.noreply.github.com>
2017-12-15 16:12:15 +0800

    Update ShiroConfiguration.java

--- src/main/java/com/pf/org/cms/common/ShiroConfiguration.java
+++ src/main/java/com/pf/org/cms/common/ShiroConfiguration.java
@@ -1,10 +1,15 @@
 package com.pf.org.cms.common;
 
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.realm.Realm;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,25 +25,37 @@ public class ShiroConfiguration {
     private static final Logger log = LoggerFactory.getLogger(ShiroConfiguration.class);
 
     /**
-     * 注册Realm
+     * 注入Realm
+     * @return MyRealm
+     */
+    @Bean(name = "myRealm")
+    public MyRealm myAuthRealm() {
+        MyRealm myRealm = new MyRealm();
+        log.info("myRealm注册完成");
+        return myRealm;
+    }
+
+
+    /**
+     * 注入SecurityManager
+     * @param myRealm
      * @return SecurityManager
      */
-    @Bean
-    public SecurityManager securityManager() {
+    @Bean(name = "securityManager")
+    public SecurityManager securityManager(@Qualifier("myRealm")MyRealm myRealm) {
         DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
-        MyRealm myRealm = new MyRealm();
         manager.setRealm(myRealm);
-        log.info("SecurityManager注册完成");
+        log.info("securityManager注册完成");
         return manager;
     }
 
     /**
-     * 注册Filter
+     * 注入Filter
      * @param securityManager
      * @return ShiroFilterFactoryBean
      */
-    @Bean
-    public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
+    @Bean(name = "shiroFilter")
+    public ShiroFilterFactoryBean shiroFilter(@Qualifier("securityManager") SecurityManager securityManager) {
         ShiroFilterFactoryBean filterFactoryBean = new ShiroFilterFactoryBean();
         filterFactoryBean.setSecurityManager(securityManager);
         // 配置登录的url和登录成功的url
@@ -47,18 +64,18 @@ public class ShiroConfiguration {
         // 配置未授权跳转页面
         filterFactoryBean.setUnauthorizedUrl("/static/403.ftl");
         // 配置访问权限
-        LinkedHashMap<String, String> filterChainDefinitionMap=new LinkedHashMap<>();
+        LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
         filterChainDefinitionMap.put("/css/**", "anon"); // 表示可以匿名访问
         filterChainDefinitionMap.put("/fonts/**", "anon");
         filterChainDefinitionMap.put("/imgs/**", "anon");
         filterChainDefinitionMap.put("/js/**", "anon");
         filterChainDefinitionMap.put("/auth/**", "anon");
-        filterChainDefinitionMap.put("/demo/**","anon");
+        filterChainDefinitionMap.put("/demo/**", "anon");
         filterChainDefinitionMap.put("/*", "authc");// 表示需要认证才可以访问
         filterChainDefinitionMap.put("/**", "authc");
         filterChainDefinitionMap.put("/*.*", "authc");
         filterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
-        log.info("ShiroFilterFactoryBean注册完成");
+        log.info("shiroFilter注册完成");
         return filterFactoryBean;
     }
 
