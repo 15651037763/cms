25d8703bd7fc0c4e5f76b09af91b9496473790e2
pf <33946003+15651037763@users.noreply.github.com>
2017-12-19 15:55:38 +0800

    Update cms_insert.sql

--- src/main/sql/cms_insert.sql
+++ src/main/sql/cms_insert.sql
@@ -1,5 +1,5 @@
-INSERT INTO `cms_user_info` VALUES (1, 'admin', '管理员', '123', '测试备注', 'admin', '2017-12-8 20:32:02', 'admin', '2017-12-8 20:32:09', 0, 1);
-INSERT INTO `cms_user_info` VALUES (2, '17040406', '张三', '111', '测试备注', 'admin', '2017-12-8 20:32:02', 'admin', '2017-12-8 20:32:09', 1, 1);
+INSERT INTO `cms_user_info` VALUES (1, 'admin', '管理员', 'd0970714757783e6cf17b26fb8e2298f', '测试备注', 'admin', '2017-12-8 20:32:02', 'admin', '2017-12-8 20:32:09', 0, 1);
+INSERT INTO `cms_user_info` VALUES (2, '17040406', '张三', 'd0970714757783e6cf17b26fb8e2298f', '测试备注', 'admin', '2017-12-8 20:32:02', 'admin', '2017-12-8 20:32:09', 1, 1);
 
 INSERT INTO `cms_role_info` VALUES (1, 'admin', '系统管理员', 'admin', '2017-12-8 19:06:35', 'admin', '2017-12-8 19:06:39', 0, 1);
 INSERT INTO `cms_role_info` VALUES (2, 'guest', '客人', 'admin', '2017-12-8 19:06:35', 'admin', '2017-12-8 19:06:35', 1, 1);
