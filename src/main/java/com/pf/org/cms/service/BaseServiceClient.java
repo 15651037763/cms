f8741a4683aacead6323fc725229432b9ca181c7
pf <33946003+15651037763@users.noreply.github.com>
2018-02-09 17:44:00 +0800

    Update BaseServiceClient.java

--- src/main/java/com/pf/org/cms/service/BaseServiceClient.java
+++ src/main/java/com/pf/org/cms/service/BaseServiceClient.java
@@ -9,6 +9,36 @@ import java.util.List;
  */
 public interface BaseServiceClient {
     /**
+     * 保存
+     * @param obj
+     * @return
+     */
+    public int insert(Object obj);
+
+    /**
+     * 根据long型主键查询
+     * @param id
+     * @param c
+     * @param <T>
+     * @return
+     */
+    public <T> T query(int id, Class<T> c);
+
+    /**
+     * 根据主键更新
+     * @param obj
+     * @return
+     */
+    public int update(Object obj);
+
+    /**
+     * 根据主键删除
+     * @param obj
+     * @return
+     */
+    public int delete(Object obj);
+
+    /**
      * 通用批量保存
      *
      * @param sqlId
@@ -16,4 +46,5 @@ public interface BaseServiceClient {
      * @return
      */
     public int batchInsert(String sqlId, List data);
+
 }
