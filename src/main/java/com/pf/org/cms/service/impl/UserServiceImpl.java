315589cf2ab33dab63c3681997271e11f370b183
pf <33946003+15651037763@users.noreply.github.com>
2018-02-10 17:53:09 +0800

    Update UserServiceImpl.java

--- src/main/java/com/pf/org/cms/service/impl/UserServiceImpl.java
+++ src/main/java/com/pf/org/cms/service/impl/UserServiceImpl.java
@@ -3,10 +3,14 @@ package com.pf.org.cms.service.impl;
 import com.pf.org.cms.entity.UserInfo;
 import com.pf.org.cms.entity.UserRoleInfo;
 import com.pf.org.cms.mapper.UserInfoMapper;
+import com.pf.org.cms.service.BaseServiceClient;
 import com.pf.org.cms.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +18,11 @@ import java.util.Map;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserInfoMapper userInfoMapper;
+
+    @Autowired
+    private BaseServiceClient baseServiceClient;
+
+    private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public List<UserInfo> getUserInfos(Map<String, Object> paramMap) {
@@ -26,4 +35,27 @@ public class UserServiceImpl implements UserService {
         paramMap.put("state", 1);
         return userInfoMapper.getUserRoleInfos(paramMap);
     }
+
+    @Override
+    public void addUserInfo(UserInfo userInfo) {
+        baseServiceClient.insert(userInfo);
+    }
+
+    @Override
+    public HashMap getUserInfo(long id) {
+        return baseServiceClient.query(id, UserInfo.class);
+    }
+
+    @Override
+    public int updateUserInfo(UserInfo userInfo) {
+        return baseServiceClient.update(userInfo);
+    }
+
+    @Override
+    public void deleteUserInfo(long id) {
+        UserInfo user = new UserInfo();
+        user.setId(id);
+        baseServiceClient.delete(user);
+    }
+
 }
