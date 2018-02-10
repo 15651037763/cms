4c473ca6c438afab4b95c8bfb3e66a6400ee46e8
pf <33946003+15651037763@users.noreply.github.com>
2018-02-10 17:53:30 +0800

    Update UserService.java

--- src/main/java/com/pf/org/cms/service/UserService.java
+++ src/main/java/com/pf/org/cms/service/UserService.java
@@ -3,12 +3,14 @@ package com.pf.org.cms.service;
 import com.pf.org.cms.entity.UserInfo;
 import com.pf.org.cms.entity.UserRoleInfo;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public interface UserService {
     /**
-     * 获取用户信息
+     * 批量获取用户信息
+     *
      * @param paramMap
      * @return List<UserInfo>
      */
@@ -16,8 +18,38 @@ public interface UserService {
 
     /**
      * 获取用户角色信息
+     *
      * @param paramMap
      * @return List<UserRoleInfo>
      */
     public List<UserRoleInfo> getUserRoleInfos(Map<String, Object> paramMap);
+
+    /**
+     * 新增用户信息
+     *
+     * @param userInfo
+     */
+    public void addUserInfo(UserInfo userInfo);
+
+    /**
+     * 获取用户信息
+     *
+     * @param id
+     * @return
+     */
+    public HashMap getUserInfo(long id);
+
+    /**
+     * 更新用户信息
+     *
+     * @param userInfo
+     */
+    public int updateUserInfo(UserInfo userInfo);
+
+    /**
+     * 删除用户信息
+     *
+     * @param id
+     */
+    public void deleteUserInfo(long id);
 }
