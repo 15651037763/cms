684fe820cd66ba39c48691e10c4e1b8be2db159d
pf <33946003+15651037763@users.noreply.github.com>
2018-02-27 20:12:53 +0800

    增加restful接口

--- src/main/java/com/pf/org/cms/web/AdminController.java
+++ src/main/java/com/pf/org/cms/web/AdminController.java
@@ -1,8 +1,15 @@
 package com.pf.org.cms.web;
 
+import com.pf.org.cms.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * @Auther: pf
@@ -12,9 +19,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/admin")
 public class AdminController {
+    @Autowired
+    UserService userService;
+
     @ResponseBody
     @RequestMapping(value = "/testAuthorization")
     public String testAuthorization() {
         return "你拥有admin访问权限";
+    }
+
+    @ApiOperation(value = "getUser", notes = "管理员接口获取用户")
+    @RequestMapping(method = RequestMethod.GET, value = "/user")
+    public Map getUser(@RequestParam long userId) {
+        return userService.getUserInfo(userId);
+    }
+
+    @ApiOperation(value = "addUser", notes = "管理员接口新增用户")
+    @RequestMapping(method = RequestMethod.POST, value = "/user")
+    public Map addUser(@RequestParam long userId) {
+        return userService.getUserInfo(userId);
+    }
+
+    @ApiOperation(value = "updateUser", notes = "管理员接口更新用户")
+    @RequestMapping(method = RequestMethod.PUT, value = "/user")
+    public Map updateUser(@RequestParam long userId) {
+        return userService.getUserInfo(userId);
+    }
+
+    @ApiOperation(value = "delUser", notes = "管理员接口删除用户")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/user")
+    public Map delUser(@RequestParam long userId) {
+        return userService.getUserInfo(userId);
     }
 }
