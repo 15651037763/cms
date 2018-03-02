23405ea56f9edeab37c72f9ed5c393114abd180d
pf <33946003+15651037763@users.noreply.github.com>
2018-03-02 17:57:36 +0800

    Update BaseServiceClientImpl.java

--- src/main/java/com/pf/org/cms/service/impl/BaseServiceClientImpl.java
+++ src/main/java/com/pf/org/cms/service/impl/BaseServiceClientImpl.java
@@ -3,6 +3,7 @@ package com.pf.org.cms.service.impl;
 import com.pf.org.cms.annotation.Column;
 import com.pf.org.cms.annotation.Id;
 import com.pf.org.cms.annotation.Table;
+import com.pf.org.cms.exception.CmsException;
 import com.pf.org.cms.mapper.BaseMapper;
 import com.pf.org.cms.service.BaseServiceClient;
 import org.apache.ibatis.session.ExecutorType;
@@ -39,50 +40,81 @@ public class BaseServiceClientImpl implements BaseServiceClient {
     @Autowired
     SqlSessionTemplate sqlSessionTemplate;
 
-    private Map<String, Object> transformObj(Object t) {
+    private Map<String, Object> transformObj(Object t, String type) {
         //获取表名
         if (null == t.getClass().getAnnotation(Table.class)) {
-            throw new RuntimeException("Error Input Object! Error @Table Annotation.");
+            throw new CmsException("Error Input Object! Error @Table Annotation.");
         }
         Map<String, Object> re = new HashMap<String, Object>();
         re.put("TABLE_NAME", t.getClass().getAnnotation(Table.class).value());
 
         Method[] m = t.getClass().getMethods();
         if (null == m || m.length <= 0) {
-            throw new RuntimeException("Error Input Object! No Method.");
+            throw new CmsException("Error Input Object! No Method.");
         }
-
-        List k = new ArrayList();//存放列名
-        List v = new ArrayList();//存放列值
-        for (Method i : m) {
-            //获取列名和值
-            try {
+        //insert数据结构
+        if ("insert".equals(type)) {
+            List k = new ArrayList();//存放列名
+            List v = new ArrayList();//存放列值
+            for (Method i : m) {
+                //获取列名和值
                 if (null != i.getAnnotation(Column.class)) {
                     k.add(i.getAnnotation(Column.class).value());
-                    v.add(i.invoke(t, null));
+                    v.add(getInvokeValue(t, i));
                     continue;
                 }
                 if (null != i.getAnnotation(Id.class)) {
                     re.put("KEY_ID", i.getAnnotation(Id.class).value());
-                    re.put("KEY_VALUE", i.invoke(t, null));
+                    re.put("KEY_VALUE", getInvokeValue(t, i));
                 }
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException("Error Input Object! Error Invoke Get Method.", e);
-            } catch (InvocationTargetException e) {
-                throw new RuntimeException("Error Input Object! Error Invoke Get Method.", e);
             }
-        }
-        re.put("COLUMNS", k);
-        re.put("VALUES", v);
-        if (k.size() != v.size()) {
-            throw new RuntimeException("Error Input Object! Internal Error.");
+            if (k.size() != v.size()) {
+                throw new CmsException("Error Input Object! Internal Error.");
+            }
+            re.put("COLUMNS", k);
+            re.put("VALUES", v);
+        } else if ("update".equals(type)) {
+            //update数据结构
+            List d = new ArrayList();
+            for (Method i : m) {
+                Map<String, Object> map = new HashMap<>();
+                if (null != i.getAnnotation(Column.class) && null != getInvokeValue(t, i)) {
+                    map.put("COLUMN", i.getAnnotation(Column.class).value());
+                    map.put("COL_VALUE", getInvokeValue(t, i));
+                    d.add(map);
+                    continue;
+                }
+                if (null != i.getAnnotation(Id.class)) {
+                    re.put("KEY_ID", i.getAnnotation(Id.class).value());
+                    re.put("KEY_VALUE", getInvokeValue(t, i));
+                }
+            }
+            re.put("DATA", d);
+        } else if ("common".equals(type)) {
+            //common数据结构
+            for (Method i : m) {
+                if (null != i.getAnnotation(Id.class)) {
+                    re.put("KEY_ID", i.getAnnotation(Id.class).value());
+                    re.put("KEY_VALUE", getInvokeValue(t, i));
+                }
+            }
         }
         return re;
     }
 
+    private Object getInvokeValue(Object t, Method i) {
+        try {
+            return i.invoke(t, null);
+        } catch (IllegalAccessException e) {
+            throw new CmsException("Error Input Object! Error Invoke Get Method.", e);
+        } catch (InvocationTargetException e) {
+            throw new CmsException("Error Input Object! Error Invoke Get Method.", e);
+        }
+    }
+
     @Override
     public int insert(Object obj) {
-        Map<String, Object> params = transformObj(obj);
+        Map<String, Object> params = transformObj(obj, "insert");
         log.info(new StringBuffer("Function Insert.Transformed Params:").append(params).toString());
         return baseMapper.insert(params);
     }
@@ -91,15 +123,12 @@ public class BaseServiceClientImpl implements BaseServiceClient {
     public HashMap query(long id, Class c) {
         Map<String, Object> params = new HashMap<>();
         try {
-            params = transformObj(c.newInstance());
+            params = transformObj(c.newInstance(), "common");
             log.info(new StringBuffer("Function Query.Transformed Params:").append(params).toString());
         } catch (InstantiationException e) {
-            e.printStackTrace();
+            throw new CmsException("Common Query Error.", e);
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        if (null == params.get("KEY_ID")) {
-            throw new RuntimeException("主键为空");
+            throw new CmsException("Common Query Error.", e);
         }
         params.put("KEY_VALUE", id);
         return baseMapper.queryForObject(params);
@@ -107,27 +136,21 @@ public class BaseServiceClientImpl implements BaseServiceClient {
 
     @Override
     public int update(Object obj) {
-        Map<String, Object> params = transformObj(obj);
+        Map<String, Object> params = transformObj(obj, "update");
         log.info(new StringBuffer("Function Update.Transformed Params:").append(params).toString());
-        if (null == params.get("KEY_ID") || null == params.get("KEY_VALUE")) {
-            throw new RuntimeException("主键或主键值为空");
-        }
         return baseMapper.update(params);
     }
 
     @Override
     public int delete(Object obj) {
-        Map<String, Object> params = transformObj(obj);
+        Map<String, Object> params = transformObj(obj, "common");
         log.info(new StringBuffer("Function Delete.Transformed Params:").append(params).toString());
-        if (null == params.get("KEY_ID") || null == params.get("KEY_VALUE")) {
-            throw new RuntimeException("主键或主键值为空");
-        }
         return baseMapper.delete(params);
     }
 
     @Override
     public int batchInsert(String sqlId, List data) {
-        /*通用批量保存，事务内每count条分批commit*/
+        /*通用批量保存，每count条分批commit*/
         if (data.isEmpty() || StringUtils.isEmpty(sqlId)) {
             log.info("批量保存出错，mapperIndex或data为空");
             return 0;
@@ -157,7 +180,7 @@ public class BaseServiceClientImpl implements BaseServiceClient {
             if (null != sqlSession) {
                 sqlSession.rollback();
             }
-            throw new RuntimeException(e);
+            throw new CmsException(e);
         } finally {
             if (null != sqlSession) {
                 sqlSession.close();
