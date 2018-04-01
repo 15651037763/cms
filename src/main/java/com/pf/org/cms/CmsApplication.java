0c97c9e93aef623c75a1b94aec6b296a04dad568
pf <33946003+15651037763@users.noreply.github.com>
2018-04-01 17:15:11 +0800

    增加定时任务demo

--- src/main/java/com/pf/org/cms/CmsApplication.java
+++ src/main/java/com/pf/org/cms/CmsApplication.java
@@ -3,6 +3,7 @@ package com.pf.org.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableScheduling
 @SpringBootApplication
 public class CmsApplication {
 
